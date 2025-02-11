7629b72afe1755ed76c0fa9516b57c8abbb13b92
Mohit SIngla <mhtkumar46@gmail.com>
2025-02-11 17:51:58 +0530

    menu changes

--- src/app/components/chat/ChatHeader.tsx
+++ src/app/components/chat/ChatHeader.tsx
@@ -63,12 +63,6 @@ const ChatHeader = () => {
                         }}
                     >
                         <ul className="py-2">
-                            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
-                                Profile
-                            </li>
-                            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
-                                Settings
-                            </li>
                             <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer" onClick={handleLogout}>
                                 Logout
                             </li>
